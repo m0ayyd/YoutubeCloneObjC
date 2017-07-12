866d4115f05f79df1615ad6ec3cfcd57799c82d6
the Luckiest <m02yyd@gmail.com>
2017-07-12 21:35:46 +0300

    Initial Commit

---
+++ YoutubeCloneObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YoutubeCloneObjC
+//
+//  Created by the Luckiest on 7/12/17.
+//  Copyright © 2017 the Luckiest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YoutubeCloneObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YoutubeCloneObjC
+//
+//  Created by the Luckiest on 7/12/17.
+//  Copyright © 2017 the Luckiest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
